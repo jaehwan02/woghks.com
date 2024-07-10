f24210252778daec7818933c02fc9e73cab7753c
jaehwan02 <jhrick0129@gmail.com>
2024-07-10 22:43:26 +0900

    제목 변경 및 마우스 커서 조정

--- public/animation.js
+++ public/animation.js
@@ -72,8 +72,8 @@ gsap.to("#light2", {
 })
 
 document.addEventListener('mousemove', (e) => {
-  let mouseX = e.pageX-50; // document의 x좌표값
-  let mouseY = e.pageY-50; // document의 y좌표값
+  let mouseX = e.pageX-10; // document의 x좌표값
+  let mouseY = e.pageY-10; // document의 y좌표값
 
   let cursor = document.querySelector('.cursor');
   cursor.style.left = mouseX + 'px';
